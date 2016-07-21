f8b71fa062d7010b14438a0f92a5a66f26c7b310
renem <rene.mejiac@gmail.com>
2016-07-21 11:57:43 -0700

    + Chosen implemented

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
 		$('#side-menu').toggleClass('hidden-xs');
 	});
 });
+
+// Chosen Select configuration
+var config = {
+	'.chosen-select' : {},
+	'.chosen-select-deselect' : {allow_single_deselect: true},
+	'.chosen-select-no-single' : {disable_search_treshold: 10},
+	'.chosen-select-no-result' : {no_results_text: 'Oops, nothing found!'},
+	'.chosen-select-width' : {width: "95%"}
+}
+for (var selector in config) {
+	$(selector).chosen(config[selector]);
+}
